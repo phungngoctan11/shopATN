399b7edacb454e192e862cb96cf1ba9b3aaa18fc
phungngoctan11 <73920659+phungngoctan11@users.noreply.github.com>
2020-11-04 09:34:10 +0700

    Add files via upload

---
+++ controllers/product.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var router = express.Router();
+
+/// --- Code CONTROLLERs
+router.use(function timeLog (req, res, next) {
+    console.log('\n\t Product controller - Time: ', Date.now());
+    next();
+})
+
+/// ..................................................
+router.get('/product', productPage);
+
+function productPage(req, res) {
+    
+    if (session.user) 
+    {
+        MongoClient.connect(urldb, { useUnifiedTopology: true }, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("shop");
+            dbo.collection("product").find({}).toArray(function(err, productlist) {
+              if (err) throw err;
+              
+                res.render("pages/product-list",  {
+                    title: "ATN-Shop PRODUCT page", 
+                    username: session.user.username,
+                    products : productlist 
+                    , configHeader: configHeader , currpage: "Product"
+                    });
+                console.log('Found:', productlist);
+
+              db.close();
+            });
+          });
+                    
+
+        
+    } else {
+        res.redirect('/login');
+    }    
+    console.log("\n\t ... connect PRODUCT from ", req.connection.remoteAddress, req.headers.host);
+}
+
+
+/// ..................................................
+router.get('/list', listProductPage);
+function listProductPage(req, res) {
+    res.send('PRODUCT: list PRODUCT page');
+}
+
+/// --- EXports
+module.exports = router;
+
+
+/*
+
+/// ..................................................
+app.get('/product', productPage);
+function productPage(req, res) {
+    
+    if (session.user) 
+    {
+        MongoClient.connect(urldb, { useUnifiedTopology: true }, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("newshop");
+            dbo.collection("product").find({}).toArray(function(err, productlist) {
+              if (err) throw err;
+              
+                res.render("pages/product-list",  {
+                    title: "ATN-Shop PRODUCT page", 
+                    username: session.user.username,
+                    products : productlist 
+                    , configHeader: configHeader , currpage: "Product"
+                    });
+                console.log('Found:', productlist);
+
+              db.close();
+            });
+          });
+                    
+
+        
+    } else {
+        res.redirect('/login');
+    }    
+    console.log("\n\t ... connect PRODUCT from ", req.connection.remoteAddress, req.headers.host);
+}
+
+
+*/
